b4638a71f9bc5704e5a3b0723acde2271413ddba
Ho Thuy Tien <94684584+ThuyTien189@users.noreply.github.com>
2024-04-28 20:38:26 +0700

    feat: change not found page (#5)
    mqhlJVJn

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,26 +1,36 @@
+import { useNavigate } from 'react-router-dom';
 import { Box, Image, Stack } from '@mantine/core';
 
-import NotfoundImage from '@/assets/images/404.png';
+import NotfoundImage from '@/assets/images/notfound.png';
+import { Button } from '@/atoms/Button';
 import { UnSignedHeader } from '@/atoms/UnsignedHeader';
+import { PATH } from '@/constants';
 
-export const NotFoundPage = () => (
-  <Box className='h-screen'>
-    <Box className='h-headerHeight bg-navy-500 px-4 pt-4'>
-      <UnSignedHeader />
+export const NotFoundPage = () => {
+  const navigate = useNavigate();
+  return (
+    <Box className='h-screen'>
+      <Box className='h-headerHeight bg-navy-500 px-4 pt-4'>
+        <UnSignedHeader />
+      </Box>
+      <Box className='h-full w-full bg-navy-10'>
+        <Image
+          src={NotfoundImage}
+          className='relative right-[-35%] top-[5%] w-[30%]'
+        />
+        <Stack className='mt-[100px] items-center justify-center'>
+          <span className='text-5xl text-navy-900'>Whoops!</span>
+          <Button
+            onClick={() => {
+              navigate(PATH.OVERVIEW_PAGE);
+            }}
+            variant='outline'
+            title='Back to home'
+            size='md'
+            className='w-[200px] rounded-3xl'
+          />
+        </Stack>
+      </Box>
     </Box>
-    <Box className='h-full w-full bg-navy-10 pt-10'>
-      <Stack className='items-center justify-center'>
-        <span className='text-5xl text-navy-900'>Whoops!</span>
-        <span className='text-center text-xl text-navy-900'>
-          Unfortunately, the page you were looking for could not be found. It
-          may be temporarily <br /> unavailable, moved or no longer exists.
-          Please check your spelling and retry.
-        </span>
-      </Stack>
-      <Image
-        src={NotfoundImage}
-        className='relative bottom-[-15%] right-[-360px] w-[80vw]'
-      />
-    </Box>
-  </Box>
-);
+  );
+};
