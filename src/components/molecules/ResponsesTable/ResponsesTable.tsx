d617e9ce1e83dcbe3fb875a6d5c56111c729d727
Hương Trinh <90098037+TranThiHuongTrinh@users.noreply.github.com>
2024-05-10 19:24:13 +0700

    Feat/7 tk6 ayga (#16)
    * feat: send notification by email when someone send response
    
    KvacDBu0
    
    * feat: create settings leftbar
    
    7TK6Ayga

--- src/components/molecules/ResponsesTable/ResponsesTable.tsx
+++ src/components/molecules/ResponsesTable/ResponsesTable.tsx
@@ -62,8 +62,7 @@ export const ResponsesTable = (props: ResponsesTableProps) => {
       ),
     [sortStatus, responseRows],
   );
-  console.log('responseRow', responseRows);
-  console.log('records', records);
+
   useEffect(() => {
     setParams({ ...params, page: currentPage });
   }, [currentPage]);
@@ -96,9 +95,8 @@ export const ResponsesTable = (props: ResponsesTableProps) => {
       })),
     ],
     [elementIdAndNameList],
-  );
-  console.log('columns', columns);
-
+  );  
+  
   return (
     <div className='px-4'>
       <DataTable
