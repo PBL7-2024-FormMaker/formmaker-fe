235c46cc614c2cc75c425cc58e7becad88cd3dcf
Ho Thuy Tien <94684584+ThuyTien189@users.noreply.github.com>
2024-05-21 12:10:48 +0700

    fix: add env variables (#29)
    sMifPSlt

--- src/configs/envConstants.ts
+++ src/configs/envConstants.ts
@@ -2,3 +2,6 @@ export const APP_PORT = Number(import.meta.env.VITE_APP_PORT) || 5173;
 
 export const BACK_END_URL =
   import.meta.env.VITE_BACK_END_URL || `http://localhost:3000`;
+
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const API_KEY = import.meta.env.VITE_API_KEY;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,6 @@
 interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly VITE_CLIENT_ID: string;
   readonly VITE_BACK_END_URL: string;
   readonly VITE_APP_PORT: number?;
 }
